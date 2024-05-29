ccda590f898ef63ff73fac852d1d239b450d3da3
Shashank Awasthi <103922153+awasthishashank@users.noreply.github.com>
2024-05-30 00:51:54 +0530

    Update Redux.js by 5

--- src/store/Redux.js
+++ src/store/Redux.js
@@ -2,9 +2,9 @@ import {createStore} from 'redux'
 const counterReducer = (state = {counter:0} , action) =>{
     switch (action.type) {
         case 'increment':
-          return { counter: state.counter + 2 };
+          return { counter: state.counter + 5 };
         case 'decrement':
-          return { counter: state.counter - 2 };
+          return { counter: state.counter - 5 };
         default:
           return state;
       }
