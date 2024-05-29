9b1f0ed3121769fc038584ad2bdc83863ffbaf79
awasthishashank <awasthishashank@gmail.comgit config --global user.username awasthishashank git config --global user.email awasthishashank@gmail.com>
2024-05-30 00:41:38 +0530

    first commit

---
+++ src/store/Redux.js
@@ -0,0 +1,16 @@
+import {createStore} from 'redux'
+const counterReducer = (state = {counter:0} , action) =>{
+    switch (action.type) {
+        case 'increment':
+          return { counter: state.counter + 2 };
+        case 'decrement':
+          return { counter: state.counter - 2 };
+        default:
+          return state;
+      }
+
+}
+
+const store = createStore(counterReducer);
+
+export default store;
